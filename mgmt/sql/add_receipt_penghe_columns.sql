bf7909f5a7fa240ac7f4445eda408b17fd1f1c42
yangwang39 <yangwang39@trip.com>
2025-12-07 12:50:05 +0800

    优化案件信息表，新增案件归属地字段，调整收款单号数据类型

--- mgmt/sql/add_receipt_penghe_columns.sql
+++ mgmt/sql/add_receipt_penghe_columns.sql
@@ -2,4 +2,6 @@
 ALTER TABLE management_system.case_info ADD COLUMN receipt_number INT NULL COMMENT '收款单号(提交结案审核时生成,起始818)';
 ALTER TABLE management_system.case_info ADD COLUMN penghe_case_number INT NULL COMMENT '澎和案件号(司法确认结案方式生成,起始689)';
 ALTER TABLE case_info MODIFY COLUMN receipt_number VARCHAR(16);
-
+ALTER TABLE management_system.case_info
+    MODIFY COLUMN case_location ENUM('九堡','彭埠','笕桥','本部','四季青','闸弄口','凯旋街道')
+    COMMENT '案件归属地';
